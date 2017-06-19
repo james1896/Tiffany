2ba035f200a0704e382b02aa9b52172c7275fba3
james <james@gmail.com>
2017-06-19 10:45:50 +0800

    药品详情

---
+++ app/src/main/java/com/tiffany/com/tiffany/medicine/MedicineDetailActivity.java
@@ -0,0 +1,17 @@
+package com.tiffany.com.tiffany.medicine;
+
+import android.os.Bundle;
+
+import com.tiffany.com.tiffany.BaseActivity;
+import com.tiffany.com.tiffany.R;
+
+/**
+ * Created by moses on 19/06/2017.
+ */
+
+public class MedicineDetailActivity extends BaseActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.medicine_detail_activity);
+    }
+}
